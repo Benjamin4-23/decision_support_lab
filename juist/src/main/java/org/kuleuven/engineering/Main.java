94a704a4ab1e1e568b9af497f8ff36b55504665e
Arne Vanmarcke <arne.vanmarcke2001@gmail.com>
2024-10-09 17:04:47 +0200

    Added comments in mainn

--- juist/src/main/java/org/kuleuven/engineering/Main.java
+++ juist/src/main/java/org/kuleuven/engineering/Main.java
@@ -5,8 +5,8 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "I3_3_1_5.json";
-        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        String fileName = "I15_16_1_3.json";
+        Path currentPath = Paths.get(System.getProperty("user.dir")); // current location
         Path filePath = Paths.get(currentPath.toString(), "juist", "data", fileName);
         Warehouse warehouse = DataReader.read(filePath);
         System.out.println(warehouse);
